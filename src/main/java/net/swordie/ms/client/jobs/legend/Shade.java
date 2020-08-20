41211e8c3deeb4147c96c00d54880056b4f9304e
Descended <41481611+Descended@users.noreply.github.com>
2020-08-21 01:11:21 +0200

    Fixed Shade & Luminous Missing beginner skills

--- src/main/java/net/swordie/ms/client/jobs/legend/Shade.java
+++ src/main/java/net/swordie/ms/client/jobs/legend/Shade.java
@@ -63,6 +63,8 @@ public class Shade extends Job {
     public static final int SPIRIT_INCARNATION = 25121030;
 
     private int[] addedSkills = new int[] {
+            FOX_TROT,
+            SPIRIT_BOND_I,
     };
 
     private final int[] buffs = new int[]{
