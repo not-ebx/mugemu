3b2c708b0e01cc8c85a6c3762739ea4a57dddf57
Bratah123 <brandonnguyen3301@gmail.com>
2021-04-15 22:17:15 -0700

    Fixed Dark Shock (Battle Mage) not dealing damage (skill id typo)

--- src/main/java/net/swordie/ms/handlers/user/AttackHandler.java
+++ src/main/java/net/swordie/ms/handlers/user/AttackHandler.java
@@ -526,7 +526,7 @@ public class AttackHandler {
                 ai.x = inPacket.decodeShort();
                 ai.y = inPacket.decodeShort();
             }
-            if (header == InHeader.USER_MAGIC_ATTACK && skillID != 3211016 && !SkillConstants.isKinesisPsychicAreaSkill(skillID)) {
+            if (header == InHeader.USER_MAGIC_ATTACK && skillID != 32111016 && !SkillConstants.isKinesisPsychicAreaSkill(skillID)) {
                 short forcedX = inPacket.decodeShort();
                 short forcedY = inPacket.decodeShort();
                 boolean dragon = inPacket.decodeByte() != 0;
