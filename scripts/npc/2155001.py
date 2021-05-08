71603f55a9ef183efd34bb58b031478e49f9a4c8
Pyonta <furi.curi36@gmail.com>
2021-05-07 21:51:50 -0400

    * Implement Absolab and Stigma coin exchange * Implement Absolab shop, expanded Stigma shop (Stigma shop's Frozen Katara: ID typo when generating table? 1342102 vs 1342101)

---
+++ scripts/npc/2155001.py
@@ -0,0 +1,3 @@
+# Three-Hands (2155001) | Haven (310070000)
+
+sm.openShop(2155001)
---
+++ scripts/npc/npc_1540893.py
@@ -0,0 +1,37 @@
+# Quartermaster Sakaro (2155009) | Deserted Camp (105300000)
+
+faintStigma = 4001868
+twistedStigma = 4001869
+stigmaCoin = 4310199
+
+if sm.hasItem(faintStigma, 50) and sm.hasItem(twistedStigma):
+    #How many coins can the user exchange up to?
+    faintQuantity = sm.getQuantityOfItem(faintStigma)
+    faintQuotient = faintQuantity//50
+    twistedQuantity = sm.getQuantityOfItem(twistedStigma)
+    if faintQuotient > twistedQuantity:
+        purchaseCap = twistedQuantity
+    else:
+        purchaseCap = faintQuotient
+    
+    sm.sendNext("Ah, a Spirit Stone marked by the stigma of vengeance.")
+    sm.sendNext("Give to me 50 #i" + str(faintStigma) + "##z" + str(faintStigma)
+    + "# and 1 #i" + str(twistedStigma) + "##z" + str(twistedStigma) + "#,\r\n"
+    + "and I'll give you 1 #i" + str(stigmaCoin) + "##z" + str(stigmaCoin) + "# in return."
+    + "What do you say?\r\n"
+    + "#L0# #i" + str(stigmaCoin) + "##z" + str(stigmaCoin) + "##l")
+    quantity = sm.sendAskNumber("You can get up to " + str(purchaseCap) + " #b#z" + str(stigmaCoin) + "#(s)#k. How many do you want to trade? \r\n"
+    + "(#t" + str(faintStigma) + "# in your possession: " + str(faintQuantity) + ")\r\n"
+    + "(#t" + str(twistedStigma) + "# in your possession: " + str(twistedQuantity) + ")\r\n", 1, 1, 100)
+
+    if not sm.canHold(stigmaCoin):
+        sm.sendSayOkay("Please make room in your Etc. inventory.")
+    elif purchaseCap < quantity:
+        sm.sendSayOkay("You don't have enough Spirit Stones.")
+    else:
+        sm.consumeItem(faintStigma, quantity*50)
+        sm.consumeItem(twistedStigma, quantity)
+        sm.giveItem(stigmaCoin, quantity)
+else:
+    sm.sendSayOkay("Come to me when you have 50 #i" + str(faintStigma) + "##z" + str(faintStigma) +
+    "#s and a #i" + str(twistedStigma) + "##z" + str(twistedStigma) + "#.")
