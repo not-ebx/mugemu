3ef5cc3af67847ce5da7ca3a6b5291505ab2dd7c
Pyonta <furi.curi36@gmail.com>
2021-05-26 00:32:20 -0400

    * Handle Mihile's Echo of Hero via quest script * Mihile's 3rd/4th Job Advance quests actually clear after advancing * Edit dialogue across Mihile's Job Advance quests and tutorial * Mihile can now skip his tutorial after the initial encounter with Neinhart (this can re-fire by entering this specific map again from the attic/relogging *without* having Limbert's quest there) * Fix Mihile tutorial UI script re-running after accepting Limbert's quest (or not hasQuestCompleted => and not hasQuestCompleted; the former will return True since you'll only ever be in that map during its part of the tutorial)

--- scripts/quest/q20320e.py
+++ scripts/quest/q20320e.py
@@ -1,16 +1,15 @@
+# Knight's Qualification Exam (20320) | Mihile 3rd Job
+
 sm.setSpeakerID(1101002)
-if sm.sendAskYesNo("Now you're a REAL knight. Would you like to take your Job Advancement?"):
-    if not sm.canHold(1142401):
-        sm.sendSayOkay("You need inventory space.")
-        sm.dispose()
-    else:
+response = sm.sendAskYesNo("Now you're a REAL knight. Would you like to take your Job Advancement?")
+if response:
+    if sm.canHold(1142401):
         if chr.getJob() == 5110:
             sm.jobAdvance(5111)
             sm.giveItem(1142401)
-            sm.dispose()
+            sm.completeQuest(parentID)
         else:
-            sm.sendNext("You are not a mihile class.")
-            sm.dispose()
-else:
-    sm.dispose()
-
+            sm.sendSayOkay("You are not a 2nd job Mihile.")
+    else:
+        sm.sendSayOkay("Please make room in your Equip inventory.")
+    
--- scripts/quest/q20810s.py
+++ scripts/quest/q20810s.py
@@ -1,18 +1,18 @@
 # 20810 - [Job Adv] (Lv.30)   Mihile
-sm.setSpeakerID(2520025)
-if sm.sendAskYesNo("Congratulations on passing your trials, do you want to become one of my knights?"):
-    if not sm.canHold(1302038):
-        sm.sendSayOkay("You need inventory space.")
 
-    elif not sm.canHold(1142400):
-        sm.sendSayOkay("You need inventory space.")
-    else:
+sm.setSpeakerID(2520025)
+response = sm.sendAskYesNo("Congratulations on passing your trials, Mihile. "
+"Are you prepared to be declared an official apprentice knight?")
+if response:
+    if sm.getEmptyInventorySlots(1) >= 2:
         if chr.getJob() == 5100:
             sm.jobAdvance(5110)
             sm.giveItem(1302038)
             sm.giveItem(1142400)
-            sm.completeQuest(20810)
-            sm.dispose()
+            sm.completeQuest(parentID)
+            sm.sendSayOkay("I now pronounce you an Apprentice Knight! "
+            "I've given you some SP to use. Make us proud.")
         else:
-            sm.sendNext("You are not a mihile class.")
-            sm.dispose()
+            sm.sendSayOkay("You are not a 1st job Mihile.")
+    else:
+        sm.sendSayOkay("Please make room in your Equip inventory.")
