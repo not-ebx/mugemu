e2baeeaf082c45d4f95c29157ef13a75c22620fd
Pyonta <furi.curi36@gmail.com>
2021-12-30 12:02:39 -0500

    * Implement Edelstein Black Portal with option to use 10k meso instead of becoming useless after exhausting Black Wings Tokens * More mob drop SQL fixes

--- scripts/reactor/muruengItem2.py
+++ scripts/reactor/muruengItem2.py
@@ -5,6 +5,6 @@ note = 2022252
 
 drop = sm.getDropInRect(peach, 100)
 if drop is not None:
-    sm.dropItem(note, 894, -93)
     field.removeDrop(drop.getObjectId(), 0, False, -1)
+    sm.dropItem(note, 894, -93)
     
